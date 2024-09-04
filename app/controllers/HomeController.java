a8eab7ccd289c066d85aa6fd912a7cfe1aab2816
takuzo <takuya.koiso.s@gmail.com>
2024-09-04 10:25:32 +0900

    add PostForm with CSRF

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,9 +1,13 @@
 package controllers;
 
+import form.PostForm;
 import model.Post;
+import play.data.Form;
+import play.data.FormFactory;
 import play.mvc.*;
 import service.PostService;
 
+import javax.inject.Inject;
 import java.util.Collections;
 import java.util.List;
 
@@ -12,6 +16,13 @@ import java.util.List;
  * to the application's home page.
  */
 public class HomeController extends Controller {
+
+    private final Form<PostForm> postForm;
+
+    @Inject
+    public HomeController(FormFactory formFactory) {
+        this.postForm = formFactory.form(PostForm.class);
+    }
 
     /**
      * An action that renders an HTML page with a welcome message.
@@ -23,15 +34,19 @@ public class HomeController extends Controller {
         return ok(views.html.index.render());
     }
 
-    public Result getPosts(Integer postId) {
+    public Result getPosts(Integer postId, Http.Request request) {
         PostService postService = new PostService();
         if(postId == null){
-            return ok(views.html.posts.render(postService.getPosts()));
+            return ok(views.html.posts.render(postService.getPosts(), postForm, request));
         }
         Post post = postService.getPost(postId);
         if (post == null){
-            return ok(views.html.posts.render(Collections.emptyList()));
+            return ok(views.html.posts.render(Collections.emptyList(), postForm, request));
         }
-        return ok(views.html.posts.render(List.of(post)));
+        return ok(views.html.posts.render(List.of(post), postForm, request));
+    }
+
+    public Result createPost(Http.Request request) {
+        return badRequest();
     }
 }
